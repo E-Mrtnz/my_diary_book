f38c3f24483a17adff0f55fd5af237f1e94d4adb
E-Mrtnz <edgar4ntonio_1999@hotmail.com>
2022-07-21 08:49:11 -0600

    ningun cambio

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'dart:io' as io;
+import 'package:permission_handler/permission_handler.dart';
+import 'package:path_provider/path_provider.dart';
 
 void main() {
   runApp(const MyApp());
@@ -36,6 +39,73 @@ class _MyHomePageState extends State<MyHomePage> {
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[],
         ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.repeat_rounded),
+        onPressed: () async {
+          /*
+          var getDocAppDir = await getApplicationDocumentsDirectory();
+          print(getDocAppDir);
+
+          var dir = io.Directory('Master');
+          var e = await dir.exists();
+
+          var dir2 = await io.Directory('/storage/emulated/0/Download/Master/');
+
+          print(await dir2.exists());
+
+          if (!await dir2.exists()) {
+            await dir2.create(recursive: true);
+          }1
+
+          print(await dir2.exists());
+          print('object');
+          */
+
+          //var manageStorageStatus = Permission.storage.status;
+          var storageStatus = Permission.storage.status;
+          await Permission.storage.request();
+          //print(await Permission.storage.isGranted);
+
+          //
+          if (await storageStatus.isGranted) {
+            var getDocAppDir = await getExternalStorageDirectory();
+
+            var getDAD = await getApplicationDocumentsDirectory();
+
+            try {
+              if (await Permission.storage.status.isGranted) {
+                //print(await io.File('${getDocAppDir.path}/test.txt').exists());
+                //print(await getDocAppDir);
+                /*
+                print(await io.File('${getDAD.path}/test.txt').exists());
+                await io.File('${getDAD.path}/test.txt').copy(
+                    '/storage/emulated/0/Android/data/com.example.my_diary_book/files/test.txt');
+                */
+                print(await io.File('${getDocAppDir!.path}/test.txt').exists());
+                print(await io.Directory('${getDocAppDir.path}/mi').exists());
+                io.Directory dir = io.Directory('${getDocAppDir.path}/mer');
+                if (!await dir.exists()) {
+                  dir.create(recursive: true);
+                  print('kiwi');
+                } else {
+                  print('delete');
+                }
+                print(getDocAppDir);
+
+                print(getDAD.path);
+              }
+            } catch (e) {
+              print(e);
+            }
+          }
+          //var manageExternalStorage = Permission.manageExternalStorage;
+          //await manageExternalStorage.request();
+
+          //print(await dir2.exists());
+
+          //print('${getDocAppDir.path}/$e');
+        },
       ),
     );
   }
